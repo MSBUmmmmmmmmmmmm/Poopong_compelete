7b7df7c57ebf1174c60b4a0caafdcad607ec6301
MSBUmmmmmmmmmmmm <168351930+MSBUmmmmmmmmmmmm@users.noreply.github.com>
2024-04-30 11:02:02 +0800

    Add files via upload

--- basic.py
+++ basic.py
@@ -1,81 +1,136 @@
-#Element的logic方法暂时只有几个低级原件在用，以后可能有别的用处
-class Unactived(Exception):
+# Element的logic方法暂时只有几个低级原件在用，以后可能有别的用处
+class Inactivated(Exception):
     def __init__(self):
         super().__init__()
 
+
+class Timer:
+    def __init__(self, start: int = 0, step: int = 1):
+        self.start = start
+        self.step = step
+        self._count = start
+
+    def add(self):
+        self._count += self.step
+
+    def write(self, val: int):
+        self._count = val
+
+    def read(self):
+        return self._count
+
+    def reset(self):
+        self.write(self.start)
+
+
 class Pin:
-    
-    def __init__(self, value:int=0, to:list=None, actived:bool=False):
-        if to is None:
-            to = []
-        self.value = value
-        self.actived = actived
-        self.to = to
+    list_pins = []
+
+    def __init__(self, value: int = 0, target: list = None,
+                 activity: bool = False, lock: bool = False, last: list = None):
+        if target is None:
+            target = []
+        if last is None:
+            last = []
+        self._value = value
+        self.activity = activity
+        self.target = target
+        self.last = last
+        self.lock = lock
+        Pin.list_pins.append(self)
 
     def connect(self, obj):
-        self.to.append(obj)
+        self.target.append(obj)
+        obj.last.append(self)
 
-    def main(self):
-        self.actived = True
-        for obj in self.to:
-            try:
-                obj.value = self.value
-            finally:
-                obj.main()
+    def work(self):
+        self.activity = True
+        self.lock = False
+        for obj in self.last:
+            if not obj.activity:
+                self.lock = True
+                break
+        if not self.lock:
+            for obj in self.target:
+                if isinstance(obj, Pin):
+                    obj._value = self._value
+                try:
+                    obj.work()
+                except Inactivated:
+                    pass
+
+    def write(self, val: int):
+        self._value = val
+
+    def read(self):
+        return self._value
+
+    @staticmethod
+    def reset():
+        for pin in Pin.list_pins:
+            pin._value = 0
+            pin.activity = False
+            pin.lock = False
 
 
 class Element:
-
-    def __init__(self, inpins:tuple=None, outpins:tuple=None):
+    def __init__(self, inpins: tuple = None, outpins: tuple = None):
         if inpins is None:
             inpins = ()
         if outpins is None:
             outpins = ()
-        for i in inpins:
-            i.connect(self)
         self.inpins = inpins
         self.outpins = outpins
+        for pin in self.inpins:
+            pin.connect(self)
 
-    
-    def main(self):
+    def work(self):
         try:
-            for inpin in self.inpins:
-                if not inpin.actived:
-                    raise Unactived()
+            for pin in self.inpins:
+                if not pin.activity:
+                    raise Inactivated()
             self.logic()
-            for outpin in self.outpins:
-                outpin.main()
-        except Unactived:
+            for pin in self.outpins:
+                pin.work()
+        except Inactivated:
             pass
 
     def logic(self):
+        print("So why a father needs to work personally??")
         pass
 
-    
 
-class Delay(Element):
+# Reg 中的inpins依次为写入，是否读，是否写
+class Reg(Element):
     def __init__(self):
-        in_pins = tuple([Pin()])
-        out_pins = tuple([Pin()])
-        super().__init__(inpins=in_pins, outpins=out_pins)
-        self.stored = 0
+        super().__init__(inpins=tuple([Pin(), Pin(), Pin()]), outpins=tuple([Pin()]))
+        self._stored = 0
 
     def logic(self):
-        self.outpins[0].value = self.stored
-        self.stored = self.inpins[0].value
+        if self.inpins[1] == 1:
+            self.outpins[0].write(self.read())
+        if self.inpins[2] == 1:
+            self.write(self.inpins[0].read())
+
+    def write(self, val):
+        self._stored = val
+
+    def read(self):
+        return self._stored
 
 
 class Not(Element):
-    
+
     def __init__(self):
         in_pins = tuple([Pin()])
         out_pins = tuple([Pin()])
         super().__init__(inpins=in_pins, outpins=out_pins)
 
     def logic(self):
-        self.outpins[0].value = 0
-        if self.inpins[0].value == 0:
-            self.outpins[0].value = 1
+        self.outpins[0].write(0)
+        if self.inpins[0].read() == 0:
+            self.outpins[0].write(1)
+
 
 class And(Element):
 
@@ -85,15 +140,14 @@ class And(Element):
         super().__init__(inpins=in_pins, outpins=out_pins)
 
     def logic(self):
-        self.outpins[0].value = 0
-        if self.inpins[0].value == 1:
-            if self.inpins[1].value == 1:
-                self.outpins[0].value = 1
-
+        self.outpins[0].write(0)
+        if self.inpins[0].read() == 1:
+            if self.inpins[1].read() == 1:
+                self.outpins[0].write(1)
 
 
 class Nand(Element):
-    
+
     def __init__(self):
         gate_and = And()
         gate_not = Not()
@@ -105,10 +159,12 @@ class Nand(Element):
         gate_and.outpins[0].connect(gate_not.inpins[0])
         gate_not.outpins[0].connect(self.outpins[0])
 
+    def logic(self):
+        pass
 
 
 class Or(Element):
-    
+
     def __init__(self):
         gate_not0 = Not()
         gate_not1 = Not()
@@ -121,6 +177,9 @@ class Or(Element):
         gate_not0.outpins[0].connect(gate_nand.inpins[0])
         gate_not1.outpins[0].connect(gate_nand.inpins[1])
         gate_nand.outpins[0].connect(self.outpins[0])
+
+    def logic(self):
+        pass
 
 
 class Nor(Element):
@@ -135,6 +194,10 @@ class Nor(Element):
         gate_or.outpins[0].connect(gate_not.inpins[0])
         gate_not.outpins[0].connect(self.outpins[0])
 
+    def logic(self):
+        pass
+
+
 class And3(Element):
 
     def __init__(self):
@@ -148,6 +211,9 @@ class And3(Element):
         gate_and0.outpins[0].connect(gate_and1.inpins[0])
         self.inpins[2].connect(gate_and1.inpins[1])
         gate_and1.outpins[0].connect(self.outpins[0])
+
+    def logic(self):
+        pass
 
 
 class Or3(Element):
@@ -164,21 +230,25 @@ class Or3(Element):
         self.inpins[2].connect(gate_or1.inpins[1])
         gate_or1.outpins[0].connect(self.outpins[0])
 
+    def logic(self):
+        pass
+
 
 def TruthTable(gate_class):
     test_gate = gate_class()
     length = len(test_gate.inpins)
-    situations = pow(2,length)
+    situations = pow(2, length)
     print(f"Table name:{gate_class}")
     for i in range(situations):
-        changed = "0"*length + bin(i)[2:]
+        changed = "0" * length + bin(i)[2:]
         input_values = []
         for j in range(length):
-            test_gate.inpins[j].value = int(changed[j-length])
-            test_gate.inpins[j].main()
-            input_values.append(test_gate.inpins[j].value)
-        out_values = [k.value for k in test_gate.outpins]
+            test_gate.inpins[j].write(int(changed[j - length]))
+            test_gate.inpins[j].work()
+            input_values.append(test_gate.inpins[j].read())
+        out_values = [pin.read() for pin in test_gate.outpins]
         print(f"in:{input_values}, out:{out_values}")
+    Pin.reset()
 
 
 if __name__ == "__main__":
