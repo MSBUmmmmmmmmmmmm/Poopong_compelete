e201b6ad855c0063268fa807d7c78628ae25e25f
MSBUmmmmmmmmmmmm <168351930+MSBUmmmmmmmmmmmm@users.noreply.github.com>
2024-05-01 23:09:00 +0800

    add Xor and XNor

---
+++ alu.py
@@ -0,0 +1,31 @@
+import basic
+from basic import TruthTable as TT1
+import eight
+
+
+class Half_adder(basic.Element):
+
+    def __init__(self, debug: bool = False):
+        gate_Xor = basic.Xor(debug=True)
+        gate_and = basic.And()
+        in_pins = basic.Pin.spawn(2)
+        out_pins = basic.Pin.spawn(2)
+        super().__init__(inpins=in_pins, outpins=out_pins, debug=debug)
+        self.inpins[0].connect(gate_and.inpins[0], gate_Xor.inpins[0])
+        self.inpins[0].connect(gate_and.inpins[1], gate_Xor.inpins[1])
+        gate_Xor.outpins[0].connect(self.outpins[0])
+        gate_and.outpins[0].connect(self.outpins[1])
+
+    def logic(self):
+        pass
+
+
+if __name__ == "__main__":
+    while True:
+        print(">>>", end="")
+        try:
+            exec(input())
+        #except Exception as e:
+        #    print(e)
+        finally:
+            pass
